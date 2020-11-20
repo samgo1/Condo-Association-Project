e5139a255bcc5e707849fb75b65843220f789bfb
Radley Carpio <radleycarpio@live.com>
2020-11-19 19:47:42 -0500

    OUTLINE

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" href="./css/index.css">
         <link rel="stylesheet" href="./css/materialize.css">
         <link rel="stylesheet" href="./css/materialize.min.css">
+        <link rel="stylesheet" href="./css/components/post.css">
         <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
     </head>
     <body onload="show('dashboard')">
@@ -18,7 +19,9 @@
                 <button class="btn-flat" onClick="show('dashboard')">Dashboard</button>
                 <button class="btn-flat" onClick="show('mail')">Mail</button>
                 <button class="btn-flat" onClick="show('groups')">Groups</button>
+                <button class="btn-flat" onClick="show('condos')">Members</button>
                 <button class="btn-flat" onClick="show('condos')">Condos</button>
+                <button class="btn-flat" onClick="show('condos')">Requests</button>
                 <button class="btn-flat" onClick="show('logout')">Logout</button>
             </div>
             <div class="mainview" id="mainView"></div>
---
+++ views/requests.php
@@ -0,0 +1,1 @@
+<h1>REQUESTS</h1>
