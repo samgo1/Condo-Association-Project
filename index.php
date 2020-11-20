c12c025461254f5c532900a76c0986462072e81d
Radley Carpio <radleycarpio@live.com>
2020-11-19 19:33:18 -0500

    default page dashboard

--- index.php
+++ index.php
@@ -9,19 +9,20 @@
         <link rel="stylesheet" href="./css/materialize.min.css">
         <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
     </head>
-    <body>
+    <body onload="show('dashboard')">
         <div class="gridcontainer">
             <div class="header">
                 <img src="./assets/con logo.png" alt="">
             </div>
             <div class="menubar">
                 <button class="btn-flat" onClick="show('dashboard')">Dashboard</button>
+                <button class="btn-flat" onClick="show('mail')">Mail</button>
                 <button class="btn-flat" onClick="show('groups')">Groups</button>
                 <button class="btn-flat" onClick="show('condos')">Condos</button>
                 <button class="btn-flat" onClick="show('logout')">Logout</button>
             </div>
             <div class="mainview" id="mainView"></div>
         </div>
-        <script src="index.js"></script>
     </body>
+    <script src="index.js"></script>
 </html>
