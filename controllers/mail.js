d9f73f5f6734838ced8a2f10077a24922240b7d4
Radley Carpio <radleycarpio@live.com>
2020-12-09 00:52:58 -0500

    update mail.js

--- controllers/mail.js
+++ controllers/mail.js
@@ -12,17 +12,14 @@ function showMessageItem(senderName, senderID, dateSent, content){
 
     var messageArea = document.getElementById('messageArea');
 
-    messageArea.innerHTML = `
-    <div class=\"message\">
-    <div class=\"content\">
-       <div class=\"sender\">
-            From:
-            <span name=\"sender\">` + senderName + ` ` + senderID + `</span>
-        </div>
-        <div name=\"subject\" class=\"subject\">` + dateSent + `</div>
-    </div>
-    <div name=\"content\" class=\"content section\">`+ content + `
-    </div>
-    </div>
-    `;
+    messageArea.innerHTML = "<div class=\"message\">" +
+    "<div class=\"content\">" +
+       "<div class=\"sender\">" +
+            "From:" +
+            "<span name=\"sender\">" + senderName + " " + senderID + "</span>" +
+        "</div>" +
+        "<div name=\"subject\" class=\"subject\">" + dateSent + "</div>" +
+    "</div>" +
+    "<div name=\"content\" class=\"content section\">" + content + 
+    "</div></div>";
 }
