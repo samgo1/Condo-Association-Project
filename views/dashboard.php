2b732d05379e86dc7e9075beeb2d651367a1f2a0
Radley Carpio <radleycarpio@live.com>
2020-12-09 14:31:01 -0500

    Fixed bug where login wouldn't appear

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,6 +1,7 @@
-<?php session_start() ?>
+<?php session_start(); ?>
 <div class="dashboard">
 <?php
+
 include_once 'var.php';
 $conn = mysqli_connect($servername,$username,$password,$dbname);
 
@@ -8,30 +9,23 @@ if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   exit();
 }
-?>
 
-<div class="userInfo">
-
-<?php 
 if(!isset($_SESSION['signed_in']) || $_SESSION['signed_in'] == false)
 {
 
-  header('Location: https://192.168.1.16/fp/login/includes/loginbtn.inc.php');
+  //header('Location: https://192.168.1.16/fp/login/includes/loginbtn.inc.php');
   include '../login/includes/loginbtn.inc.php';
 
 
 }//connected
 else {
 
-    echo '<h3> Welcome, ' . $_SESSION['name'] . '</h3>';
+    echo "<div class=\"userInfo\"><h2> Welcome, " . $_SESSION['name'] . "</h2></div>";
 
     if (isset($_SESSION["privilege"]) && $_SESSION['privilege'] === 'admin') {
         include '../components/dashboard/admin.php';
     }
-?>
-</div>
 
-<?php
   $member_id = $_SESSION['id'];
 
   // get the groups
@@ -42,15 +36,19 @@ else {
   $groups = $result->fetch_all();
   $num_of_groups = sizeof($groups);
   // get post informations
-  $sql = "SELECT post_id FROM `post_visibility` pv JOIN post p ON pv.post_id=p.id WHERE member_id = \"  {$member_id}  \" ORDER BY date_time DESC ";
+  $sql = "SELECT post_id FROM `post_visibility` WHERE member_id = \"  {$member_id}  \" ";
 
   $result0 = $conn->query($sql);
 
+  $rows = [];
+
   while ($row = $result0->fetch_assoc()){
+
+    array_push($rows, $row);
     // get post
     $post_id = $row['post_id'];
     $sql = "SELECT date_time, permission, content_text, content_img, author_id FROM `post`
-          WHERE id=\" {$post_id} \" ORDER BY date_time DESC";
+          WHERE id=\" {$post_id} \" ";
 
     $result2 = $conn->query($sql);
     $post = $result2->fetch_row();
@@ -61,7 +59,6 @@ else {
     $content_text = $post[2];
     $content_img = $post[3];
     $author_id = $post[4];
-
     $sql = "SELECT name FROM `member` WHERE id ='{$author_id}'";
     $author_name = $conn->query($sql)->fetch_row()[0];
     include "../components/dashboard/post.php";
